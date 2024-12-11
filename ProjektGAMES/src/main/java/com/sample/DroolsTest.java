c0c15f2b649817fadb55b2f670ea480b3794623a
Jan Zajac <138476188+Szogunisko@users.noreply.github.com>
2024-12-11 21:03:25 +0100

    Update DroolsTest.java

--- ProjektGAMES/src/main/java/com/sample/DroolsTest.java
+++ ProjektGAMES/src/main/java/com/sample/DroolsTest.java
@@ -4,13 +4,13 @@ import org.kie.api.KieServices;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
 
+import javax.swing.JFrame;
 
-/**
- * This is a sample class to launch a rule.
- */
+
 public class DroolsTest {
 
     public static final void main(String[] args) {
+    	JFrame frame = new JFrame();
         try {
             // load up the knowledge base
 	        KieServices ks = KieServices.Factory.get();
@@ -18,41 +18,10 @@ public class DroolsTest {
         	KieSession kSession = kContainer.newKieSession("ksession-rules");
 
             // go !
-            Message message = new Message();
-            message.setMessage("Hello World");
-            message.setStatus(Message.HELLO);
-            kSession.insert(message);
             kSession.fireAllRules();
         } catch (Throwable t) {
             t.printStackTrace();
         }
+        frame.setLayout(null);
     }
-
-    public static class Message {
-
-        public static final int HELLO = 0;
-        public static final int GOODBYE = 1;
-
-        private String message;
-
-        private int status;
-
-        public String getMessage() {
-            return this.message;
-        }
-
-        public void setMessage(String message) {
-            this.message = message;
-        }
-
-        public int getStatus() {
-            return this.status;
-        }
-
-        public void setStatus(int status) {
-            this.status = status;
-        }
-
-    }
-
 }
